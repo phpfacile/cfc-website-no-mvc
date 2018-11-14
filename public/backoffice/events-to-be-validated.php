da07a281304d551aec71d85d09cf821b7d9333b5
HEUTE Damien <dev@phpfacile.com>
2018-11-14 18:20:44 +0100

    * EVOL Now a list of events (to be validated) is available

--- public/backoffice/events-to-be-validated.php
+++ public/backoffice/events-to-be-validated.php
@@ -10,6 +10,8 @@ use PHPFacile\Geocoding\Db\Service\LocationService;
 
 use Zend\Db\Adapter\Adapter;
 
+$escaper = new Zend\Escaper\Escaper('utf-8');
+
 $cfg = include(__DIR__.'/../../config/autoload/local.php');
 
 $adapter         = new Adapter($cfg['CFC']['adapter']);
@@ -26,17 +28,30 @@ if (0 === count($eventSubmissions)) {
     die();
 }
 ?>
-<table>
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<table class="table">
   <tr>
     <th>Proposé le</th>
     <th>Par</th>
+    <th>Email</th>
+    <th>Evénement</th>
+    <th>Type</th>
+    <th>URL</th>
+    <th>Ville</th>
+    <th>Pays</th>
   </tr>
 <?php
 foreach ($eventSubmissions as $eventSubmission) {
 ?>
   <tr>
-    <td><?= $eventSubmission->insertionDateTimeUTC;?></td>
+    <td><?= $eventSubmission->submissionDateTimeUTC;?></td>
     <td><?php echo $escaper->escapeHtml($eventSubmission->submitter->name);?></td>
+    <td><?php echo $escaper->escapeHtml($eventSubmission->submitter->email);?></td>
+    <td><?php echo $escaper->escapeHtml($eventSubmission->event->name);?></td>
+    <td><?php echo $escaper->escapeHtml($eventSubmission->event->type);?></td>
+    <td><?php echo $escaper->escapeHtml($eventSubmission->event->url);?></td>
+    <td><?php echo $escaper->escapeHtml($eventSubmission->event->place->name);?></td>
+    <td><?php echo $escaper->escapeHtml($eventSubmission->event->place->country->code);?></td>
   </tr>
 <?php
 }
